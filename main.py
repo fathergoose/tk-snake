c233eb0224f562a5d528ebf2d21868fc5faf299f
Al Ilseman <al@ilseman.com>
2023-02-17 21:10:46 -0600

    Initial balck screen

---
+++ main.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+
+class Snake(tk.Canvas):  # "SnakeGame"
+    def __init__(self):
+        super().__init__(
+            width=600, height=620, background="black", highlightthickness=0
+        )
+
+        self.pack()
+
+
+
+root = tk.Tk()
+root.title("TK-Snake")
+root.resizable(False, False)
+root.tk.call("tk", "scaling", 4.0)
+
+board = Snake()
+
+root.mainloop()
+
